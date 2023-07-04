21965a37d843f1514524020b8c2da24b3244ba9a
Gisang Lee <48293283+bobopack@users.noreply.github.com>
2023-07-04 13:11:48 +0900

    Time: 164 ms (93.49%) | Memory: 18 MB (32.17%) - LeetSync

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -2,7 +2,8 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         from collections import Counter
         val, count = Counter(nums).most_common()[0]
-        if count > len(nums) / 2:
-            return val
-        else:
-            return -1
+        return val
+        # if count > len(nums) / 2:
+        #     return val
+        # else:
+        #     return -1
