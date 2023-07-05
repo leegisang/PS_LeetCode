8751e76123d85f0e060a3cc003e6c0e2adf0231f
Gisang Lee <48293283+bobopack@users.noreply.github.com>
2023-07-05 09:35:30 +0900

    Time: 908 ms (93.61%) | Memory: 27.1 MB (98.33%) - LeetSync

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         buy, sell = prices[0], 0
+        # profit = 
         poss = [sell - buy]
 
         for i in range(1, len(prices)):
@@ -12,7 +13,10 @@ class Solution:
             else:
                 if price > sell:
                     sell = price
+        print(buy, sell)
         poss.append(sell - buy)
+        print(poss)
         final = max(poss)
         return final if final > 0 else 0
+    # max를 0으로 두자
     
