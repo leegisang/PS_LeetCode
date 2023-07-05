a63acca7dd8e7f59e99ff3d2a51340e5c1a98327
Gisang Lee <48293283+bobopack@users.noreply.github.com>
2023-07-05 09:19:59 +0900

    Time: 165 ms (92.06%) | Memory: 18 MB (32.20%) - LeetSync

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,9 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        from collections import Counter
-        val, count = Counter(nums).most_common()[0]
-        return val
+        # from collections import Counter
+        # val, count = Counter(nums).most_common()[0]
         # if count > len(nums) / 2:
         #     return val
         # else:
         #     return -1
+        nums.sort()
+        return nums[len(nums)//2]
+    # 정렬하고 가운데 엘리먼트 리턴
