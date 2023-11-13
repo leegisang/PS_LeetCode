eb200131c9308d2a824dd401867a90a2bcacf214
Gisang Lee <bobopack@kaist.ac.kr>
2023-11-13 23:35:19 +0900

    Time: 46 ms (53.27%) | Memory: 16.6 MB (21.12%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,11 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         ranked_paper_list = sorted(citations, reverse=True)
-        paper_num = len(ranked_paper_list)
-        # if paper_num == 0:
-        #     return 0
-        for i in range(paper_num):
-            ith_citations = ranked_paper_list[i]
-            if ith_citations < i+1:
+        for i in range(len(ranked_paper_list)):
+            if ranked_paper_list[i] < i+1:
                 return i
-        return paper_num
+        return i + 1
